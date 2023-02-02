2ea2bf5a647b7235f7d7158700beb41a017f7fa4
MSAdministrator <MSAdministrator@users.noreply.github.com>
2023-02-02 11:25:05 -0600

    ci: Updating testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,2 @@
 """Configuration for the pytest test suite."""
 import pytest
-from click.testing import CliRunner
-
-
-@pytest.fixture
-def runner() -> CliRunner:
-    """Fixture for invoking command-line interfaces."""
-    return CliRunner()
